f7bacdf463e4dd5f48cdb6a89e9f19fca7f3f37e
marlic <marlic-git@phu.pl>
2015-07-26 12:42:37 +0200

    changes from master

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('zzz-term-log')(__dirname);
 
-var _ = require("underscorek");
+var _ = require("underscorek"); //@1.0.0
 _.test('1.0.0');
 
 var	abc = require("library-abc"); // underscorek-extend@3.0.0
@@ -11,3 +11,5 @@ var	xyz = require("library-xyz"); // underscorek-extend@2.0.0
 xyz.test('1.0.0-ext-2.0.0');
 _.test('1.0.0');
 
+var	zzz = require("library-zzz"); // underscorek@1.0.0
+zzz.test('1.0.0');
